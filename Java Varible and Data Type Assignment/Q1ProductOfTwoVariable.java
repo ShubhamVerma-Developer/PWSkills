608ce032df91f1371b801f47ee81b9bffc5831a3
shubham <shubham25122001@gmail.com>
2023-01-13 13:50:23 +0530

    Q1

---
+++ Java Varible and Data Type Assignment/Q1ProductOfTwoVariable.java
@@ -0,0 +1,12 @@
+/*Q1 - Take 2 integer values in two variables x and y and print their product.
+Sample Input : x=2, y=4 (Both integers)
+Sample Output : 8*/
+
+public class Q1ProductOfTwoVariable {
+    public static void main(String[] args) {
+       int x = 2;
+       int y = 4;
+       int prod = x*y;
+       System.out.println(prod);
+    }
+}
