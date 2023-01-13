221fc18ade2b49ee6231f81632b2b2b3277e83d4
shubham <shubham25122001@gmail.com>
2023-01-13 12:46:27 +0530

    Scanner

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class EvenOdd{
     public static void main(String[] args) {
-        int n=4;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         if(n%2==0){
             System.out.println("Even");
         }
         else{
             System.out.println("odd");
         }
+
+
     }
 }
