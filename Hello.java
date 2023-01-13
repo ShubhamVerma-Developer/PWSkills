5e44fda13f267ee087388a092b8b88dc754c6da8
shubham <shubham25122001@gmail.com>
2023-01-13 12:41:52 +0530

    next

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 class Hello{
     public static void main(String[] args) {
         System.out.println("Hello java");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i+" ");
+        }
     }
 }
