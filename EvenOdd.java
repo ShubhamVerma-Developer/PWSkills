c90be991feb90a3cc874a6979ec6d114f3007706
shubham <shubham25122001@gmail.com>
2023-01-13 12:44:30 +0530

    next

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd{
+    public static void main(String[] args) {
+        int n=4;
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
